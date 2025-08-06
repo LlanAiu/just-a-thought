7861f09f2ad34470f53fbb5900f2d86c361faa4a
Alan Liu <liualan998@gmail.com>
2025-08-06 17:43:10 -0400

    [feature] example component

--- frontend/app/routes/new.tsx
+++ frontend/app/routes/new.tsx
@@ -1,9 +1,37 @@
 // builtin
 
+import { useState, type ChangeEvent } from "react";
+
 // external
 
 // internal
 
 export default function NewThought() {
-    return <div>New Thought</div>;
+    const [text, setText] = useState<string>("");
+
+    function handleTextChange(e: ChangeEvent<HTMLTextAreaElement>) {
+        const newText: string = e.target.value;
+        if (allowChange(text, newText)) {
+            setText(s => newText);
+        }
+    }
+
+    function allowChange(oldString: string, newString: string): boolean {
+        const sameStart: boolean = newString.startsWith(oldString);
+        const addedChars: boolean = newString.length > oldString.length;
+        return sameStart && addedChars;
+    }
+
+    return (
+        <div>
+            <h1>New Thought</h1>
+
+            <textarea
+                name="thought"
+                value={text}
+                onChange={handleTextChange}
+            />
+
+        </div>
+    );
 }
