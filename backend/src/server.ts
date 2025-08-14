a205511849536cfeed4024fef37ee2decc10098b
Alan Liu <liualan998@gmail.com>
2025-08-14 13:30:20 -0400

    [feature] add cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,19 +5,27 @@ import "./bootstrap.js";
 
 // external
 import fastify, { type FastifyInstance } from "fastify";
+import cors from "@fastify/cors";
 
 // internal
 import { setupRoutes } from "./routes.js";
 
+const ALLOWED_ORIGINS = process.env.FRONTEND_URL;
+if (!ALLOWED_ORIGINS) throw new Error("ALLOWED_ORIGINS environment variable not set!");
+
 
 const server: FastifyInstance = fastify();
 
+await server.register(cors, {
+    origin: [ALLOWED_ORIGINS],
+    credentials: true
+});
+
 server.get("/", async (_req, _res) => {
     return { hello: "World!" };
-})
+});
 
 setupRoutes(server);
-
 
 
 server.listen({ port: 8080 }, (err, address) => {
